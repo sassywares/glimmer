ae30b23eb38024f65e52f1a4609a0766639d81aa
thekayshawn <kashanahmad.me@gmail.com>
2024-07-18 23:39:47 +0500

    refactor: rename components as per conventions

---
+++ src/modules/shared/components/toggle-group.component.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import * as React from "react";
+
+import { toggleVariants } from "./toggle.component";
+
+import { VariantProps } from "class-variance-authority";
+import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
+
+import { cn } from "@/modules/shared/shared.utils";
+
+const ToggleGroupContext = React.createContext<
+  VariantProps<typeof toggleVariants>
+>({
+  size: "default",
+  variant: "default",
+});
+
+const ToggleGroup = React.forwardRef<
+  React.ElementRef<typeof ToggleGroupPrimitive.Root>,
+  VariantProps<typeof toggleVariants> &
+    React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root>
+>(({ size, variant, children, className, ...props }, ref) => (
+  <ToggleGroupPrimitive.Root
+    ref={ref}
+    className={cn("flex items-center justify-center gap-1", className)}
+    {...props}
+  >
+    <ToggleGroupContext.Provider value={{ size, variant }}>
+      {children}
+    </ToggleGroupContext.Provider>
+  </ToggleGroupPrimitive.Root>
+));
+
+ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName;
+
+const ToggleGroupItem = React.forwardRef<
+  React.ElementRef<typeof ToggleGroupPrimitive.Item>,
+  VariantProps<typeof toggleVariants> &
+    React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item>
+>(({ size, variant, children, className, ...props }, ref) => {
+  const context = React.useContext(ToggleGroupContext);
+
+  return (
+    <ToggleGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        toggleVariants({
+          size: context.size || size,
+          variant: context.variant || variant,
+        }),
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </ToggleGroupPrimitive.Item>
+  );
+});
+
+ToggleGroupItem.displayName = ToggleGroupPrimitive.Item.displayName;
+
+export { ToggleGroup, ToggleGroupItem };
---
+++ src/modules/shared/components/typography.component.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import { cn } from "../shared.utils";
+
+import { cva, VariantProps } from "class-variance-authority";
+
+const typographyVariants = cva("", {
+  defaultVariants: {
+    variant: "inherit",
+  },
+  variants: {
+    variant: {
+      large: "text-lg font-semibold",
+      lead: "text-xl text-muted-foreground",
+      muted: "text-sm text-muted-foreground",
+      p: "leading-7 [&:not(:first-child)]:mt-6",
+      blockquote: "mt-6 border-l-2 pl-6 italic",
+      small: "text-sm font-medium leading-none",
+      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
+      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+      inherit: "text-inherit font-inherit leading-inherit tracking-inherit",
+      h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+      code: "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+    },
+  },
+});
+
+type Props<T extends React.ElementType> = {
+  as?: T;
+  className?: string;
+  children: React.ReactNode;
+};
+
+export type TypographyProps<T extends React.ElementType> = Props<T> &
+  VariantProps<typeof typographyVariants> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof Props<T>>;
+
+const Typography = React.forwardRef(
+  <T extends React.ElementType = "div">(
+    { as, variant, children, className = "", ...rest }: TypographyProps<T>,
+    ref: React.Ref<any>,
+  ) => {
+    const Component = as || "div";
+
+    return (
+      <Component
+        ref={ref}
+        {...rest}
+        className={cn(typographyVariants({ variant, className }))}
+      >
+        {children}
+      </Component>
+    );
+  },
+);
+
+Typography.displayName = "Typography";
+
+export { Typography };
