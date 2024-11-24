af664c7981f01f55e37e0ed3f36558ab105d8ffe
thekayshawn <kashanahmad.me@gmail.com>
2024-11-24 12:07:32 +0500

    chore: minor sorting

--- src/config.ts
+++ src/config.ts
@@ -6,29 +6,29 @@ export type WindowWithConfig = typeof window & {
 };
 
 export type Config = {
-  baseUrl: string;
-  serviceUrl: string;
   i18n: typeof i18nConfig;
+  baseUrl: string;
   defaults: {
     staleTimeMs: number;
     copyTimeoutMs: number;
     debounceIntervalMs: number;
   };
+  serviceUrl: string;
 };
 
 export const config: Config = (() => {
   const config = {
-    baseUrl: getConfig("NEXT_PUBLIC_BASE_URL", "http://localhost:3000"),
-    serviceUrl: getConfig(
-      "NEXT_PUBLIC_BASE_SERVICE_URL",
-      "http://localhost:3000/api/v1",
-    ),
     i18n: i18nConfig,
+    baseUrl: getConfig("NEXT_PUBLIC_BASE_URL", "http://localhost:3000"),
     defaults: {
       staleTimeMs: 1000 * 60 * 30, // 30 minutes,
       copyTimeoutMs: 3000,
       debounceIntervalMs: 500,
     },
+    serviceUrl: getConfig(
+      "NEXT_PUBLIC_BASE_SERVICE_URL",
+      "http://localhost:3000/api/v1",
+    ),
   } as const;
 
   if (!isServer) {
