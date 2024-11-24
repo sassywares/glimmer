2fc76e0c649ae6cd503e3508018a1391022161d0
thekayshawn <kashanahmad.me@gmail.com>
2024-11-24 23:05:28 +0500

    chore: add more power to the text component

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,10 +1,30 @@
 import { ComponentPropsWithAsChild } from "@/types";
+import { cn } from "@/utils";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, VariantProps } from "class-variance-authority";
-import * as React from "react";
+import React from "react";
 
 const textVariants = cva("", {
   variants: {
+    align: {
+      left: "text-left",
+      right: "text-right",
+      center: "text-center",
+    },
+    color: {
+      default: "text-foreground",
+      primary: "text-primary-foreground",
+      secondary: "text-secondary-foreground",
+      accent: "text-accent-foreground",
+      destructive: "text-destructive-foreground",
+      muted: "text-muted-foreground",
+    },
+    weight: {
+      normal: "font-normal",
+      medium: "font-medium",
+      semibold: "font-semibold",
+      bold: "font-bold",
+    },
     variant: {
       h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
       h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
@@ -18,6 +38,23 @@ const textVariants = cva("", {
       small: "text-sm font-medium leading-none",
       muted: "text-sm text-muted-foreground",
     },
+    truncate: {
+      true: "truncate",
+    },
+    transform: {
+      none: "normal-case",
+      uppercase: "uppercase",
+      lowercase: "lowercase",
+      capitalize: "capitalize",
+    },
+  },
+  defaultVariants: {
+    align: "left",
+    color: "default",
+    weight: "normal",
+    variant: "p",
+    truncate: false,
+    transform: "none",
   },
 });
 
@@ -27,12 +64,12 @@ export type TextProps = ComponentPropsWithAsChild<
 >;
 
 const Text = React.forwardRef<HTMLParagraphElement, TextProps>(
-  ({ variant, asChild = false, className, ...props }, ref) => {
+  ({ className, variant, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "p";
     return (
       <Comp
         ref={ref}
-        className={textVariants({ variant, className })}
+        className={cn(textVariants({ variant, className }))}
         {...props}
       />
     );
