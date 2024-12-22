d4e78fafe31dac6bc2fa489826c913765f5a6eb3
thekayshawn <kashanahmad.me@gmail.com>
2024-12-22 13:55:06 +0500

    chore: replace docs button with get started

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -43,13 +43,10 @@ export default async function LocalePage() {
               strokeLinejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-              <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-              <path d="M9 14v.01" />
-              <path d="M12 14v.01" />
-              <path d="M15 14v.01" />
+              <path d="M12 5l0 14" />
+              <path d="M5 12l14 0" />
             </svg>
-            Docs
+            Get Started
           </Button>
           <Button href="https://github.com/sassywares/glimmer">
             <svg
